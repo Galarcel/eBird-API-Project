89b47916842f07be6546c3a3d2da72281018d1b2
Galarcel <116780125+Galarcel@users.noreply.github.com>
2024-06-21 15:14:53 -0600

    Update birds.py
    brainstorming some edge cases

--- birds.py
+++ birds.py
@@ -7,6 +7,9 @@ import pandas as pd
 # returns a list of dictionaries, each containing detailed information
 # about individual bird observations from the eBird API. This includes 
 # species names, observation dates and times, locations
+#
+# normal input: region_code and API key
+# bad input: non-existent region code, API key. Extra parameters besides those in the function header or incorrect type of these
 
 def get_recent_observations(region_code, days_back=14, hotspot=False, include_provisional=False, max_results=None, spp_locale='en', api_key='YOUR_EBIRD_API_KEY'):
   url = f'https://api.ebird.org/v2/data/obs/{region_code}/recent'
@@ -24,7 +27,7 @@ def get_recent_observations(region_code, days_back=14, hotspot=False, include_pr
   headers = {
     'x-ebirdapitoken': api_key
   }
-  
+  # test for correct url construction, valid header and param dicts
   response = requests.get(url, headers=headers, params=params)
   
   if response.status_code == 200:
@@ -37,6 +40,8 @@ if __name__ == "__main__":
   region_code = 'US-CA'  # Example region code for California
   api_key = 'epcc2hvf2vd7'  # authentication is my personal API key from eBird
 
+  # testing: check for valid json file of observations (check eBird for sample return value from get request)
+  # check that outupt from data engine is correct based on requirements and SQL query specifications.
   try:
     observations = get_recent_observations(region_code, api_key=api_key)
     
